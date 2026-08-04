e97cb988b1127ff332d24f46583a22298897cb2d
vuthanhminh29 <vunhatminh2929@gmail.com>
2026-08-05 01:36:44 +0700

    remove admin navbar search

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -198,9 +198,6 @@
         <nav class="navbar navbar-expand bg-light navbar-light sticky-top px-4 py-0">
             <a href="{{ route('admin.dashboard') }}" class="navbar-brand d-flex d-lg-none me-4"><h2 class="text-primary mb-0"><i class="fa fa-hashtag"></i></h2></a>
             <a href="#" class="sidebar-toggler flex-shrink-0"><i class="fa fa-bars"></i></a>
-            <form class="d-none d-md-flex ms-4" method="get" action="{{ route('admin.products.index') }}">
-                <input class="form-control border-0" name="q" type="search" placeholder="Tìm kiếm">
-            </form>
             <div class="navbar-nav align-items-center ms-auto">
                 <div class="nav-item"><a href="{{ route('home') }}" class="nav-link"><i class="fa fa-globe me-lg-2"></i><span class="d-none d-lg-inline-flex">Website</span></a></div>
                 <div class="nav-item admin-user-box">
